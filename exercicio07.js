15d0abf8d789a4169c17b7e1513ed8213d88dc0e
walderisousa <walderi_@hotmail.com>
2025-09-15 10:54:00 -0300

    Declare três variáveis e atribua valores numéricos a elas. Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.

---
+++ exercicio07.js
@@ -0,0 +1,13 @@
+let a = 30;
+let b = 30;
+let c = 10
+
+console.log(`
+    a maior que b = ${a > b}
+    a é igual a b = ${a == b} 
+    a é maior que c = ${a > c}
+    c é maior que a = ${c > a}
+    a é maior ou igual a b = ${a >= b}
+    a é diferente de b = ${a != b} 
+    a é diferente de c = ${a != c} 
+    `)
