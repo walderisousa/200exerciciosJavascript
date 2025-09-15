e6a34699ea6e4590dc6397d41badf8d0acd8e674
walderisousa <walderi_@hotmail.com>
2025-09-15 12:00:36 -0300

    Escreva um programa que verifica a situação de um estudante de acordo com sua média final.

---
+++ exercicio12.js
@@ -0,0 +1,9 @@
+mediaFinal = parseFloat(prompt("Digite a média final do estudante"));
+
+if (mediaFinal>= 7){
+    alert("Você está aprovado!")
+} else if(mediaFinal >= 5 ){
+    alert("Você está de recuperação!")
+} else {
+    alert("Você está reprovado!")
+};
