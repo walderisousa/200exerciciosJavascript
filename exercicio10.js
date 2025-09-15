85ea633438f914f449e6b90f10e9cb890478427b
walderisousa <walderi_@hotmail.com>
2025-09-15 10:55:38 -0300

    Escreva um programa que recebe duas notas de um aluno, calcula a média e imprime se o aluno foi aprovado ou reprovado (considerando que a média para aprovação é 7).

---
+++ exercicio10.js
@@ -0,0 +1,9 @@
+nota1 = 8;
+nota2 = 7;
+media = (nota1 + nota2)/2;
+
+if(media >= 7){
+    console.log(`Você foi aprovado! Sua média = ${media}`)
+} else {
+    console.log(`Você foi reprovado! Sua média = ${media}`)
+};
