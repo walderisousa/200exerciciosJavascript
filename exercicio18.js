1770f3aa04fdb3814c549b222ff7d2316d70de94
walderisousa <walderi_@hotmail.com>
2025-09-17 12:03:02 -0300

    Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.

---
+++ exercicio18.js
@@ -0,0 +1,7 @@
+let idade = parseInt(prompt("Digite sua idade pra verificar se pode dirigir"));
+
+if (idade >= 18){
+    alert(`Você tem ${idade} anos e pode dirigir`);
+} else{
+    alert(`Você tem ${idade} anos e não pode dirigr!`)
+};
