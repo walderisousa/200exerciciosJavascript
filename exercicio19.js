14e72ac7e6ca87b303ea0860ed5d08597f593638
walderisousa <walderi_@hotmail.com>
2025-09-17 12:03:46 -0300

    Escreva um programa que verifica se um número está dentro de um determinado intervalo.

---
+++ exercicio19.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite um numero para verificar se está entre 20 e 40"));
+
+if (numero >= 20 && numero <= 40){
+    alert(`O ${numero} está dentro do intervalo de 20 a 40`);
+} else{
+    alert(`O ${numero} não está dentro do intervalo de 20 a 40`)
+};
