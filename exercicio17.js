2902497df4c8aa6e93759e1ba7353ae3d6deafd1
walderisousa <walderi_@hotmail.com>
2025-09-17 12:01:23 -0300

    Escreva um programa que determina se um número é par ou ímpar.

---
+++ exercicio17.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Digite um numero para verificar se é impar ou par"));
+
+if (numero % 2 == 0){
+    alert(`O número ${numero} é par!`);
+} else{
+    alert(`O número ${numero} é impar!`)
+};
